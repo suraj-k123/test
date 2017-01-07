b09faad3a676b57a30e1611106954a66fe462d5b
suraj-k123 <nagakuruganti@gmail.com>
2017-01-06 22:09:58 -0500

    Added assignment 2

---
+++ Student/Student/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Program
+    {
+     
+       static void Main(string[] args)
+        {
+
+            person student1= new person("123456", "abc@gmail.com", "Raju", "XYZ");
+
+            person B = new person("13243", "def@gmail.com", "bob", "james");
+
+            person c = new person();
+            c.StudentID = person.count;
+            
+           
+       
+            Console.WriteLine(student1.DOB);
+            Console.WriteLine(student1.Email);
+            Console.WriteLine(student1.LastName);
+            Console.WriteLine(student1.FirstName);
+            Console.WriteLine(student1.StudentID);
+
+            Console.WriteLine(B.DOB);
+            Console.WriteLine(B.Email);
+            Console.WriteLine(B.LastName);
+            Console.WriteLine(B.FirstName);
+            Console.WriteLine(B.StudentID);
+
+
+            Console.WriteLine(person.count);
+          
+            Console.ReadLine();
+
+                    }
+
+  
+
+
+}
+    }
+
---
+++ Student/Student/person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class person
+    {
+
+        public string DOB;
+        public string Email;
+        public string FirstName;
+        public string LastName;
+        public int StudentID;
+
+      
+       
+        public person(string DOB, string Email, string FirstName, string LastName):this()
+        {
+            this.DOB = DOB;
+            this.Email = Email;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.StudentID = count;
+
+           // for (StudentID = 1000; StudentID <= 5; StudentID++) ;
+
+          
+        }
+
+        public static int count=1000;
+
+        public static int Getcount()
+        {
+            return count-1000;
+        }
+
+        public person()
+        {
+            count++;
+        }
+
+    }
+}
